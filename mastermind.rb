bf425385589781084ef1b83c5cbb2f44037894cd
HarryCoburn <10048077+HarryCoburn@users.noreply.github.com>
2025-10-15 12:45:16 -0400

    Add input sanitization

--- mastermind.rb
+++ mastermind.rb
@@ -5,25 +5,44 @@
 # Four holes for the code.
 
 class GameBoard
-  # Shorthand for the six colors: white, black, yellow, red, blue, green
-  @@colors = %w[w b y r u g]
-
-  attr_reader :secret, :current_round, :max_rounds
+  attr_reader :secret, :current_round, :max_rounds, :colors
 
   def initialize
-    @secret = 4.times.map { @@colors.sample }
+    @colors = %w[w b y r u g] # Shorthand for the six colors: white, black, yellow, red, blue, green
+    @secret = 4.times.map { @colors.sample }
     @current_round = 1
     @max_rounds = 12
     @round_record = {}
+  end
+
+  def end_round
+    @current_round += 1
   end
 end
 
 def mastermind
   board = GameBoard.new
-  puts 'Enter your guess of four colors by inputting the capital letter: White, Black, Yellow, Red, blUe, Green: '
-  guess = gets.chomp.downcase.split('')
-  puts "Computer key: #{board.secret}"
-  puts "Your guess: #{guess}"
+  while board.current_round <= board.max_rounds
+    # get input
+    puts "Current round: #{board.current_round}"
+    puts 'Enter your guess of four colors by inputting the capital letter: White, Black, Yellow, Red, blUe, Green: '
+    guess = gets.chomp.downcase.split('')
+    if guess.length != 4
+      puts 'Incorrect number of colors entered. Try again. Do not separate colors by spaces.'
+      puts
+      next
+    elsif !guess.all? { |color| board.colors.include?(color) }
+      puts 'Incorrect color entered. Only put in the color capitals. Try again'
+      puts
+      next
+    end
+
+    puts "Computer key: #{board.secret}"
+    puts "Your guess: #{guess}"
+    puts
+    board.end_round
+  end
+  puts 'The game is over.'
 end
 
 mastermind
